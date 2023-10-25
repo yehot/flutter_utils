b122d77b95bc4f8c46883fb8a02b8fd05dcbd792
yehot <yehot2013@gmail.com>
2023-10-25 19:06:04 +0800

    fluro 路由的拦截功能

--- plugins/navigator_demo/lib/fluro_demo/main_2.dart
+++ plugins/navigator_demo/lib/fluro_demo/main_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigator_demo/fluro_demo/some_page.dart';
 import 'router_define.dart';
 
 
@@ -12,12 +13,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // onGenerateRoute 的类型是一个传入 setting 返回 Route 的 function
-      // typedef RouteFactory = Route<dynamic>? Function(RouteSettings settings);
-
       // fluro 的 router.generator :
       // Route<dynamic>? generator(RouteSettings routeSettings)
-      onGenerateRoute: Application.router.generator,
+      onGenerateRoute: (RouteSettings routeSettings) {
+        Route<dynamic>? route = Application.router.generator(routeSettings);
+
+        // 添加 fluro 之外的逻辑：
+        // 对应特定的路由 path，不使用 fluro 内置的 Route 配置
+        if (routeSettings.name == '/') {
+          // 可以添加 modal_bottom_sheet 的逻辑，给 root 页面加一个自定义的 Route
+          return MaterialPageRoute(builder: (_)=> SomePage(), settings: route?.settings);
+        }
+        // onGenerateRoute 方法是会在 push 时，动态被调用，返回一个 Route Page
+        return route;
+      },
+      // onGenerateRoute 的类型是一个传入 setting 返回 Route 的 function
+      // typedef RouteFactory = Route<dynamic>? Function(RouteSettings settings);
+      // onGenerateRoute: Application.router.generator,
     );
   }
   
--- plugins/navigator_demo/lib/fluro_demo/router_define.dart
+++ plugins/navigator_demo/lib/fluro_demo/router_define.dart
@@ -23,7 +23,13 @@ class Routes {
       handler: Handler(handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
         return const SomePage();
       }),
-      // 可以自定义转场
+      // 可以自定义转场，但是不能传一个 Route
+      // Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => const NewsPage()));
+      // modal_bottom_sheet 的实现思路是，在 root page 外包裹一个 自定义的 PageRoute （用来在 push 时，添加缩小的动画）
+      // 然后 被 push 的页面，也需要包裹到封装的 sheet 里（用来被push时，缩小的动画）
+
+      // 所以 fluro 和 modal_bottom_sheet 无法直接配合使用
+      // 对于这种的 push ，可以直接 push(MyRoute(Page))，而不使用 pushName('xx')
       transitionBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
         return child;
       },
@@ -33,7 +39,9 @@ class Routes {
     );
 
 
-
+    // router.define 会将 path 和 handler 注册到 FluroRouter 的 _routeTree 里
+    // 然后在 push 的时候，MaterialApp 的 onGenerateRoute 回调里，调用  Fluro 的 router.generator(routeSettings)
+    // 去通过 routeSettings.name 也就是 path，找到对应的 Handler (也就找到了对应的页面)
     router.define(home, handler: Handler(
       handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
         return const SomePage();
