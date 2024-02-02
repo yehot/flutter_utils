5825a3886af71ba5391598054d5289dca32c18e4
yehot <yehot2013@gmail.com>
2024-02-02 18:18:14 +0800

    PC 上修改默认 页面转场 效果

--- package/web_app/lib/src/main.dart
+++ package/web_app/lib/src/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:web_app/src/define/constants.dart';
+import 'package:web_app/src/navigation/transition/fade_page_transitions_builder.dart';
 import 'package:web_app/src/route/root.dart';
 import 'package:web_app/src/route/route_define.dart';
 
@@ -23,6 +24,16 @@ class MyApp extends StatelessWidget {
       routerConfig: _router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: ZoomPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+            // PC 上如果不修改转场，会和 iOS 转场类似
+            TargetPlatform.macOS: FadePageTransitionsBuilder(),
+            TargetPlatform.windows: FadePageTransitionsBuilder(),
+            TargetPlatform.linux: FadePageTransitionsBuilder(),
+          }
+        ),
         scaffoldBackgroundColor: bgColor,
         canvasColor: secondaryColor,
       ),
